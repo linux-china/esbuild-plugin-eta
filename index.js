8a0c453aa8051f99abf89cd0a3bc974c1297d5c9
linux_china <libing.chen@gmail.com>
2021-11-18 23:07:12 -0800

    index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const {resolve} = require('path');
+const fs = require('fs');
+const eta = require("eta")
+
+const name = 'eta'
+
+const setup = ({onResolve, onLoad}) => {
+    onResolve({filter: /\.eta$/}, ({path, resolveDir}) => ({
+        path: path,
+        namespace: 'eta-ns',
+        pluginData: {resolveDir}
+    }));
+    onLoad({filter: /\.eta$/, namespace: 'eta-ns'}, buildEtaTemplateFunction);
+}
+
+const buildEtaTemplateFunction = async ({path, pluginData}) => {
+    const resolveDir = pluginData.resolveDir;
+    const originalPath = resolve(resolveDir, path);
+    const templateText = await fs.promises.readFile(originalPath, {encoding: 'utf8'});
+    let templateFunction = eta.compile(templateText);
+    return {contents: `export default ${templateFunction.toString()}`};
+}
+
+module.exports = {name, setup};
