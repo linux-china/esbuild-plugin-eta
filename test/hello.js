dc60caa4554105a92c7258d02bb9cbc1c4c0729f
linux_china <libing.chen@gmail.com>
2021-11-27 22:28:14 -0800

    Polishing

--- test/hello.js
+++ test/hello.js
@@ -1,12 +1,14 @@
-import * as Eta from "eta";
-
+import * as eta from "eta";
+// import all partials
 import button1 from "./partials/button.eta";
 import link1 from "./partials/link.eta";
+// import template view
 import indexTemplate from "./views/index.eta";
-
-Eta.templates.define("buttons/button1", button1);
-Eta.templates.define("links/link1", link1);
-
-const output = Eta.render(indexTemplate, {name: "linux_china"})
-
+import defaultLayout from "./layout/index.eta";
+// Add partials
+eta.templates.define("buttons/button1", button1);
+eta.templates.define("links/link1", link1);
+eta.templates.define("layout", defaultLayout);
+// render template view
+const output = eta.render(indexTemplate, {name: "linux_china"})
 console.log(output);
