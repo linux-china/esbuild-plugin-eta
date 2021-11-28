5875f80efcbe55ce6108dbd06721a0ca144787d8
linux_china <libing.chen@gmail.com>
2021-11-27 23:23:37 -0800

    Polishing

--- test/hello.js
+++ test/hello.js
@@ -1,4 +1,4 @@
-import * as eta from "eta";
+import {templates, render as renderEta} from "eta";
 // import all partials
 import button1 from "./partials/button.eta";
 import link1 from "./partials/link.eta";
@@ -6,9 +6,9 @@ import link1 from "./partials/link.eta";
 import indexTemplate from "./views/index.eta";
 import defaultLayout from "./layout/index.eta";
 // Add partials
-eta.templates.define("buttons/button1", button1);
-eta.templates.define("links/link1", link1);
-eta.templates.define("layout", defaultLayout);
+templates.define("buttons/button1", button1);
+templates.define("links/link1", link1);
+templates.define("layout", defaultLayout);
 // render template view
-const output = eta.render(indexTemplate, {name: "linux_china"})
+const output = renderEta(indexTemplate, {name: "linux_china"})
 console.log(output);
